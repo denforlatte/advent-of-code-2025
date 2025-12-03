8cbda657f5f47009fae7a3f1f5342c62af0516aa
Danny Wilkins <danny.wilkins@confused.com>
2025-12-03 21:50:04 +0000

    Parse ranges

--- Day02/Part01/main.c
+++ Day02/Part01/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    char c[256];
+    FILE *fptr;
+
+    if ((fptr = fopen("input.txt", "r")) == NULL) {
+        printf("Failed to open input.txt\n");
+        return 1;
+    }
+
+    while (fscanf(fptr, "%[^,]", c) == 1) {
+        printf("Range: %s\n", c);
+
+        const int ch = fgetc(fptr);
+        if (ch == EOF) {
+            break;
+        }
+    }
+
+
+    fclose(fptr);
+
     return 0;
 }
