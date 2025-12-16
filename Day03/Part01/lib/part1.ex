af8ea57b90fabe50ffb5eb286ee57835b32d927d
Danny Wilkins <denforlatte@gmail.com>
2025-12-16 09:37:51 +0000

    Add some building block functions

--- Day03/Part01/lib/part1.ex
+++ Day03/Part01/lib/part1.ex
@@ -8,8 +8,8 @@ defmodule Part1 do
 
   ## Examples
 
-      iex> Part1.hello()
-      :world
+      iex> Part1.get_list_of_numbers("987654321111111")
+      [9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1]
 
   """
   def main do
@@ -20,5 +20,27 @@ defmodule Part1 do
     File.stream!(path)
     |> Stream.map(&String.trim/1)
     |> Enum.to_list()
+  end
+
+  def get_list_of_numbers(numberString) do
+    String.graphemes(numberString)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def get_first_highest_number(numberList) do
+    Enum.with_index(numberList)
+    |> Enum.max_by(fn {x, _} -> x end)
+  end
+
+  def get_highest_left_right_pair(numberList) do
+    {firstNumber, i} = Enum.split(numberList, length(numberList) - 1)
+    |> then(fn {head, _} -> head end)
+    |> get_first_highest_number()
+
+    {secondNumber, _} = Enum.split(numberList, i + 1)
+    |> then( fn {_, tail} -> tail end)
+    |> get_first_highest_number()
+
+    firstNumber * 10 + secondNumber
   end
 end
--- Day03/Part01/test/part1_test.exs
+++ Day03/Part01/test/part1_test.exs
@@ -5,4 +5,19 @@ defmodule Part1Test do
   test "reads test input" do
     assert Part1.read_lines("input.txt") == ["987654321111111", "811111111111119", "234234234234278", "818181911112111"]
   end
+
+  test "converts string to list of digits" do
+    assert Part1.get_list_of_numbers("987654321111111") == [9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1]
+  end
+
+  test "returns index of highest number" do
+    assert Part1.get_first_highest_number([1, 2, 3, 8, 3, 2, 8]) ==  {8, 3}
+  end
+
+  test "get highest number pair" do
+    assert Part1.get_highest_left_right_pair([9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1]) == 98
+    assert Part1.get_highest_left_right_pair([8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9]) == 89
+    assert Part1.get_highest_left_right_pair([2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8]) == 78
+    assert Part1.get_highest_left_right_pair([8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1]) == 92
+  end
 end
