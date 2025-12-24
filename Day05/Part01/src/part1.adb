5f98566b5a941b418fe008d751326299e122f5a5
Danny Wilkins <danny.wilkins@confused.com>
2025-12-24 11:12:00 +0000

    Finish day 5 part 1

--- Day05/Part01/src/part1.adb
+++ Day05/Part01/src/part1.adb
@@ -4,24 +4,25 @@ with Ada.Strings; use Ada.Strings;
 
 procedure Part1 is
    File: File_Type;
-   Line: String (1 .. 100);
+   Line: String (1 .. 200);
    Last: Natural;
    Is_Numbers: Boolean := False;
-   Ranges_Low: Array (1 .. 400) of Integer;
-   Ranges_High: Array (1 .. 400) of Integer;
-   Number_of_Ranges: Natural := 0;
-   Numbers: Array (1 .. 2000) of Integer;
-   Number_of_Numbers: Natural := 0;
+   Ranges_Low: Array (1 .. 400) of Long_Integer;
+   Ranges_High: Array (1 .. 400) of Long_Integer;
+   Number_Of_Ranges: Natural := 0;
+   Numbers: Array (1 .. 2000) of Long_Integer;
+   Number_Of_Numbers: Natural := 0;
    Array_Pointer: Natural := 1;
+   Number_Of_Matches: Natural := 0;
 begin
-   Open (File, In_File, "test-input.txt");
+   Open (File, In_File, "input.txt");
    while not End_Of_File (File) loop
       Get_Line (File, Line, Last);
 
       if Is_Numbers = True then
-         Numbers (Array_Pointer) := Integer'Value (Line (1 .. Last));
+         Numbers (Array_Pointer) := Long_Integer'Value (Line (1 .. Last));
+         Number_Of_Numbers := Array_Pointer;
          Array_Pointer := Array_Pointer + 1;
-         Number_of_Numbers := Array_Pointer;
       end if;
 
       -- This acts like a switch on the blank line to switch from parsing ranges to numbers.
@@ -34,29 +35,46 @@ begin
          declare
             Dash  : constant Positive :=
                Ada.Strings.Fixed.Index (Line (1 .. Last), "-");
-            Low : Natural;
-            High: Natural;
+            Low : Long_Integer;
+            High: Long_Integer;
          begin
-            Low  := Integer'Value (Line (1 .. Dash - 1));
-            High := Integer'Value (Line (Dash + 1 .. Last));
+            Low  := Long_Integer'Value (Line (1 .. Dash - 1));
+            High := Long_Integer'Value (Line (Dash + 1 .. Last));
 
-            -- Add the ranges to a list. 
+            -- Add the ranges to a list.
             Ranges_Low (Array_Pointer) := Low;
             Ranges_High (Array_Pointer) := High;
+            Number_Of_Ranges := Array_Pointer;
             Array_Pointer := Array_Pointer + 1;
-            Number_of_Ranges := Array_Pointer;
          end;
       end if;
    end loop;
 
-   for index in 1..Number_of_Numbers loop
-      Put_Line(Integer'Image (Numbers (index)));
+   for i in 1..Number_Of_Numbers loop
+      declare
+         Is_In_Range : Boolean := false;
+      begin
+         for j in 1..Number_Of_Ranges loop
+            if Is_In_Range = False then
+               if Numbers (i) >= Ranges_Low (j) and Numbers (i) <= Ranges_High (j) then
+                  Is_In_Range := True;
+               end if;
+            end if;
+         end loop;
+
+         if Is_In_Range then
+            Put_Line ("Is in range: " & Long_Integer'Image (Numbers (i)));
+            Number_Of_Matches := Number_Of_Matches + 1;
+         end if;
+      end;
    end loop;
+
+   Put_Line ("Number of matches: " & Integer'Image (Number_Of_Matches));
    Close (File);
 end Part1;
 
 
-
+-- 511
 
 
 -- Steps for merging ranges but I don't think I want to.
