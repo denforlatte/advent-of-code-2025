aa92210213e58cce7417b151ecaf8a8857c51739
Danny Wilkins <danny.wilkins@confused.com>
2025-12-26 10:26:16 +0000

    Flatten ranges

--- Day05/Part02/src/part2.adb
+++ Day05/Part02/src/part2.adb
@@ -2,7 +2,7 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
 with Ada.Strings; use Ada.Strings;
 
-procedure Part1 is
+procedure Part2 is
    File: File_Type;
    Line: String (1 .. 200);
    Last: Natural;
@@ -10,6 +10,9 @@ procedure Part1 is
    Ranges_Low: Array (1 .. 400) of Long_Integer;
    Ranges_High: Array (1 .. 400) of Long_Integer;
    Number_Of_Ranges: Natural := 0;
+   Ranges_Low_Collapsed: Array (1 .. 400) of Long_Integer;
+   Ranges_High_Collapsed: Array (1 .. 400) of Long_Integer;
+   Number_Of_Ranges_Collapsed: Natural := 0;
    Array_Pointer: Natural := 1;
    Number_Of_Matches: Natural := 0;
 begin
@@ -42,13 +45,62 @@ begin
       end if;
    end loop;
 
-   for j in 1..Number_Of_Ranges loop
-      
-   end loop;
+   declare
+      Low: Long_Integer;
+      High: Long_Integer;
+      Is_Merged: Boolean;
+   begin
+      for i in 1..Number_Of_Ranges loop
+         if Ranges_Low (i) /= 0 and Ranges_High (i) /= 0 then
+            Low := Ranges_Low (i);
+            High := Ranges_High (i);
+            Is_Merged := False;
 
-   Put_Line ("Number of matches: " & Integer'Image (Number_Of_Matches));
+            declare
+               Is_Changed_This_While_Loop: Boolean := True;
+               Is_Merging: Boolean := False;
+            begin
+               while Is_Changed_This_While_Loop = True loop
+                  Is_Changed_This_While_Loop := False;
+
+                  for j in i..Number_Of_Ranges loop
+                     Is_Merging := False;
+                     Put_Line ("Testing range: " & Long_Integer'Image (Ranges_Low(j)) & " -> " & Long_Integer'Image (Ranges_High (j)));
+                     if Ranges_Low (j) /= 0 and Ranges_Low (j) < Low and Ranges_High (j) > Low then
+                        Low := Ranges_Low (j);
+                        Put_Line ("New Low:" & Long_Integer'Image (Ranges_Low (j)));
+                        Is_Merging := true;
+                     end if;
+                     if Ranges_High (j) /= 0 and Ranges_High (j) > High and Ranges_Low (j) < High then
+                        High := Ranges_High (j);
+                        Put_Line ("New High:" & Long_Integer'Image (Ranges_High (j)));
+                        Is_Merging := true;
+                     end if;
+
+                     -- Zero merged ranges to "delete" then.
+                     if Is_Merging then
+                        Is_Changed_This_While_Loop := True;
+                        Is_Merged := True;
+                        Ranges_Low (j) := 0;
+                        Ranges_High (j) := 0;
+                     end if;
+                  end loop;
+               end loop;
+            end;
+
+            Ranges_Low_Collapsed (Number_Of_Ranges_Collapsed + 1) := Low;
+            Ranges_High_Collapsed (Number_Of_Ranges_Collapsed + 1) := High;
+            Number_Of_Ranges_Collapsed := Number_Of_Ranges_Collapsed + 1;
+         end if;
+      end loop;
+   end;
+
+   for i in 1..Number_Of_Ranges_Collapsed loop
+      Put_Line (Long_Integer'Image (Ranges_Low_Collapsed (i)) & " -> " & Long_Integer'Image (Ranges_High_Collapsed (i)));
+   end loop;
+   Put_Line ("Number of ranges: " & Integer'Image (Number_Of_Ranges_Collapsed));
    Close (File);
-end Part1;
+end Part2;
 
 
 -- 
@@ -59,3 +111,11 @@ end Part1;
    -- 2. For each range, loop through the REST OF the ranges.
    -- 3. If there is overlap, take the lowest start and highest end.
    -- 4. Remove the overlapping range from the list.
+
+
+
+
+-- Alternative idea
+   -- Order ranges by low
+   -- Keep a count of fresh ids
+   -- Keep a count of where we are in the ids.
